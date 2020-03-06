79cf109cf635e1a6be9f6c59aad1ae5b15fb8403
samdev18 <40631024+samdev18@users.noreply.github.com>
2020-03-06 19:04:37 -0300

    First Commit
    snakeGame

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Jogo da cobrinha</title>
+    <script src="assets/js/main.js"></script>
+</head>
+
+<body>
+
+    <canvas id="stage" width="600" height="600"></canvas>
+
+</body>
+
+</html>
